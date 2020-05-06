9fba8515af0c8ea619469fd5598ebdc47911688e
= Casey Law <= caseyjlaw@gmail.com>
2020-05-05 18:21:24 -0700

    auto update with source.steam, but seems to append

--- antmon.py
+++ antmon.py
@@ -12,20 +12,8 @@ de = dsa_store.DsaStore()
 antlist = [24] + list(range(80,100))
 ignorelist = ['ant_num', 'time', 'index']
 
-# collect data
-dfs = []
-for ant in antlist:
-    dd = de.get_dict("/mon/ant/{0}".format(ant))
-    df = pd.DataFrame.from_dict(dd, orient='index')
-    dfs.append(df)
-df = pd.concat(dfs, axis=1).transpose().reset_index()
-df.ant_num = df.ant_num.astype(str)
-df.set_index('ant_num', 0, inplace=True)
-df.columns.name = 'mp'
-df = pd.DataFrame(df[reversed(df.columns)].stack(), columns=['value']).reset_index()
-
-# add column for color coding
-minmax = {#'sim': [False, True],  # initialized directly
+# min/max range for color coding
+minmax = {'sim': [False, True],  # initialized directly
           'ant_el': [0., 145.],
 #          'time': []
           'ant_cmd_el': [0., 145.],
@@ -54,23 +42,42 @@ minmax = {#'sim': [False, True],  # initialized directly
           'fan_err': [False, True],
 #          'emergency_off': [False, True]  # what is good/bad here?
           }
-          
-color = np.where(df['mp'] == 'sim', 1, 0) * np.where(df['value'] == True, 1, 0)
-for key, value in minmax.items():
-    if isinstance(minmax[key][0], bool):
-        color += np.where(df['mp'] == key, 1, 0) * np.where(df['value'] == value[1], 1, 0)
-#    elif isinstance(minmax[key][0], int):  # something special for int valued (enumerations?)
-    else:
-        color += np.where(df['mp'] == key, 1, 0) * np.where(pd.to_numeric(df['value']) > value[1], 1, 0)
 
-df['color'] = np.array(['green', 'red'])[color]  # color for in/out of range
+# set up data
+def makedf():
+    dfs = []
+    for ant in antlist:
+        dd = de.get_dict("/mon/ant/{0}".format(ant))
+        df = pd.DataFrame.from_dict(dd, orient='index')
+        dfs.append(df)
+    df = pd.concat(dfs, axis=1).transpose().reset_index()
+    df.ant_num = df.ant_num.astype(str)
+    df.set_index('ant_num', 0, inplace=True)
+    df.columns.name = 'mp'
+    df = pd.DataFrame(df[reversed(df.columns)].stack(), columns=['value']).reset_index()
+          
+    color = np.where(df['mp'] == 'sim', 1, 0) * np.where(df['value'] == True, 1, 0)
+    for key, value in minmax.items():
+        if key == 'sim':
+            continue
+        if isinstance(minmax[key][0], bool):
+            color += np.where(df['mp'] == key, 1, 0) * np.where(df['value'] == value[1], 1, 0)
+#        elif isinstance(minmax[key][0], int):  # something special for int valued (enumerations?)
+        else:
+            color += np.where(df['mp'] == key, 1, 0) * np.where(pd.to_numeric(df['value']) > value[1], 1, 0)
+
+    df['color'] = np.array(['green', 'red'])[color]  # color for in/out of range
+    return df
+
+doc = curdoc()
+df = makedf()
 source = ColumnDataSource(df)
 
 # set up plot
 TOOLTIPS = [("value", "@value"), ("(ant_num, mp)", "(@ant_num, @mp)")]
-mplist = [mp for mp in list(df.mp.unique()[:-1]) if mp not in ignorelist]
-p = figure(plot_width=700, plot_height=1000, x_range=list(df.ant_num.unique()),
-           y_range=mplist, y_axis_label='Monitor Point', x_axis_label='Antenna Number',
+mplist = [mp for mp in list(minmax.keys()) if mp not in ignorelist]
+p = figure(plot_width=700, plot_height=1000, x_range=[str(aa) for aa in antlist],
+           y_range=list(reversed(mplist)), y_axis_label='Monitor Point', x_axis_label='Antenna Number',
            tooltips=TOOLTIPS, toolbar_location=None, x_axis_location="above",
            title="Antenna Monitor Points")
 p.rect(x='ant_num', y='mp', width=1, height=1, source=source,
@@ -78,4 +85,11 @@ p.rect(x='ant_num', y='mp', width=1, height=1, source=source,
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
 
-curdoc().add_root(p)
+def update():
+    df = makedf()
+    source.stream(df)
+    # seems to be overplotting or appending?
+
+doc.add_periodic_callback(update, 5000)
+
+doc.add_root(p)
