678d9ac04e345799a38736d2e8b6cdf7e2a46d35
= Casey Law <= caseyjlaw@gmail.com>
2020-05-11 15:53:59 -0700

    changing color scheme. more logging. including age of mp.

--- antmon.py
+++ antmon.py
@@ -5,17 +5,22 @@ from bokeh.io import show, output_file
 from bokeh.models import ColumnDataSource
 from bokeh.transform import linear_cmap
 
+import dsautils.dsa_syslog as dsl
+logger = dsl.DsaSyslogger()
+logger.subsystem('software')
+logger.app('vis')
+
 from dsautils import dsa_store
 de = dsa_store.DsaStore() 
 
 # parameters
 antlist = [24] + list(range(80,100))
-ignorelist = ['ant_num', 'time', 'index']
+ignorelist = ['ant_num', 'index']
 
 # min/max range for color coding
-minmax = {'sim': [False, True],  # initialized directly
+minmax = {'mp_age_seconds': [0, 1],
+          'sim': [False, True],  # initialized directly
           'ant_el': [0., 145.],
-#          'time': []
           'ant_cmd_el': [0., 145.],
           'drv_cmd': [0, 2],
           'drv_act': [0, 2],
@@ -45,48 +50,63 @@ minmax = {'sim': [False, True],  # initialized directly
 
 # set up data
 def makedf():
+    """ Makes a dataframe from data store (etcd).
+    returns latest measurement time in mjd.
+    column mp_age_seconds gives offset from that value.
+    """
+    
+    logger.info('Making dataframe for antenna monitor points')
     dfs = []
     for ant in antlist:
         dd = de.get_dict("/mon/ant/{0}".format(ant))
         df = pd.DataFrame.from_dict(dd, orient='index')
         dfs.append(df)
     df = pd.concat(dfs, axis=1).transpose().reset_index()
+    time_latest = df.time.max()
+    df.time = 24*3600*(time_latest - df.time)
+    df.rename(columns={'time': 'mp_age_seconds'}, inplace=True)
+
     df.ant_num = df.ant_num.astype(str)
     df.set_index('ant_num', 0, inplace=True)
     df.columns.name = 'mp'
     df = pd.DataFrame(df[reversed(df.columns)].stack(), columns=['value']).reset_index()
           
     color = np.where(df['mp'] == 'sim', 1, 0) * np.where(df['value'] == True, 1, 0)
+
+    # Define a color scheme:
+    # false/true/in/out-of-range == black/white/green/yellow
     for key, value in minmax.items():
         if key == 'sim':
             continue
+
         if isinstance(minmax[key][0], bool):
             color += np.where(df['mp'] == key, 1, 0) * np.where(df['value'] == value[1], 1, 0)
 #        elif isinstance(minmax[key][0], int):  # something special for int valued (enumerations?)
         else:
-            color += np.where(df['mp'] == key, 1, 0) * np.where(pd.to_numeric(df['value']) > value[1], 1, 0)
+            color += np.where(df['mp'] == key, 1, 0) * np.where(pd.to_numeric(df['value']) > value[1], 3, 2)
 
-    df['color'] = np.array(['green', 'red'])[color]  # color for in/out of range
-    return df
+    df['color'] = np.array(['black', 'white', 'green', 'yellow'])[color]
+    return time_latest, df
 
 doc = curdoc()
-df = makedf()
+time_latest, df = makedf()
 source = ColumnDataSource(df)
 
 # set up plot
+logger.info('Setting up plot')
 TOOLTIPS = [("value", "@value"), ("(ant_num, mp)", "(@ant_num, @mp)")]
 mplist = [mp for mp in list(minmax.keys()) if mp not in ignorelist]
 p = figure(plot_width=700, plot_height=1000, x_range=[str(aa) for aa in antlist],
            y_range=list(reversed(mplist)), y_axis_label='Monitor Point', x_axis_label='Antenna Number',
            tooltips=TOOLTIPS, toolbar_location=None, x_axis_location="above",
-           title="Antenna Monitor Points")
+           title="Antenna Monitor Points (loaded at MJD {0})".format(time_latest))
 p.rect(x='ant_num', y='mp', width=1, height=1, source=source,
        fill_color='color', alpha=0.5)
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
 
 def update():
-    df = makedf()
+    time_latest, df = makedf()
     source.stream(df, rollover=len(df))  # updates each value
 
 doc.add_periodic_callback(update, 5000)
