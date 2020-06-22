8297f4c252252429cad491149657de5149057450
= Casey Law <= caseyjlaw@gmail.com>
2020-06-22 13:37:16 -0700

    working ant and beb mps in separate plots

--- antmon.py
+++ antmon.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from bokeh.plotting import figure, curdoc
 from bokeh.io import show, output_file
+from bokeh.layouts import column
 from bokeh.models import ColumnDataSource
 from bokeh.transform import linear_cmap
 
@@ -14,11 +15,11 @@ from dsautils import dsa_store
 de = dsa_store.DsaStore() 
 
 # parameters
-antlist = list(range(1,111))
+antlist = list(range(1,130))
 ignorelist = ['ant_num', 'index']
 
 # min/max range for color coding
-minmax = {'mp_age_seconds': [0, 1],
+minmax = {'mpant_age_seconds': [0, 1],
           'sim': [False, True],  # initialized directly
           'ant_el': [0., 145.],
           'ant_cmd_el': [0., 145.],
@@ -46,6 +47,10 @@ minmax = {'mp_age_seconds': [0, 1],
 #          'lj_temp': [],
           'fan_err': [False, True],
 #          'emergency_off': [False, True]  # what is good/bad here?
+          }
+
+# beb mps
+minmax2 = {'mpbeb_age_seconds': [0, 1],
           'pd_current_a': [0.6, 2],
           'pd_current_b': [0.6, 2],
           'if_pwr_a': [-69, -60],
@@ -58,13 +63,14 @@ minmax = {'mp_age_seconds': [0, 1],
 
 # set up data
 def makedf():
-    """ Makes a dataframe from data store (etcd).
-    returns latest measurement time in mjd.
-    column mp_age_seconds gives offset from that value.
+    """ Makes dataframes (ant and beb mps) from data store (etcd).
+    returns latest measurement time in mjd for both ant and beb mps.
+    column mp*_age_seconds gives offset from that value.
     """
     
     logger.info('Making dataframe for antenna monitor points')
     dfs = []
+    dfs2 = []
     for ant in antlist:
         dd = {}
         try:
@@ -72,32 +78,47 @@ def makedf():
         except: # should be KeyDoesNotExistException
             pass
 
+        dd2 = {}
         try:
             dd2 = de.get_dict("/mon/beb/{0}".format(ant))  # beb mps
-            _ = dd2.pop('time')  # TODO: ignore for now to avoid clobbering ant time
-            dd.update(dd2)
+#            _ = dd2.pop('time')  # TODO: ignore for now to avoid clobbering ant mp time
+#            dd.update(dd2)
         except:  # should be KeyDoesNotExistException
             pass
 
         # TODO: add snap? cal?
-        # TODO: fill missing values somehow
-
         if len(dd):
             df = pd.DataFrame.from_dict(dd, orient='index')
             dfs.append(df)
 
+        if len(dd2):
+            df2 = pd.DataFrame.from_dict(dd2, orient='index')
+            dfs2.append(df2)
+
+    # ant mps
     df = pd.concat(dfs, axis=1).transpose().reset_index()
     time_latest = df.time.max()
     df.time = 24*3600*(time_latest - df.time)
-    df.rename(columns={'time': 'mp_age_seconds'}, inplace=True)
+    df.rename(columns={'time': 'mpant_age_seconds'}, inplace=True)
 
     df.ant_num = df.ant_num.astype(int).astype(str)
     df.set_index('ant_num', 0, inplace=True)
     df.columns.name = 'mp'
     df = pd.DataFrame(df[reversed(df.columns)].stack(), columns=['value']).reset_index()
-          
     color = np.where(df['mp'] == 'sim', 1, 0) * np.where(df['value'] == True, 1, 0)
 
+    # beb mps
+    df2 = pd.concat(dfs2, axis=1).transpose().reset_index()
+    time_latest = df2.time.max()
+    df2.time = 24*3600*(time_latest - df2.time)
+    df2.rename(columns={'time': 'mpbeb_age_seconds'}, inplace=True)
+
+    df2.ant_num = df2.ant_num.astype(int).astype(str)
+    df2.set_index('ant_num', 0, inplace=True)
+    df2.columns.name = 'mp'
+    df2 = pd.DataFrame(df2[reversed(df2.columns)].stack(), columns=['value']).reset_index()
+    color2 = np.where(df2['mp'] == 'sim', 1, 0) * np.where(df2['value'] == True, 1, 0)
+          
     # Define a color scheme:
     # false/true/in/out-of-range == black/white/green/yellow
     for key, value in minmax.items():
@@ -110,30 +131,55 @@ def makedf():
         else:
             color += np.where(df['mp'] == key, 1, 0) * np.where(pd.to_numeric(df['value']) > value[1], 3, 2)
 
+    for key, value in minmax2.items():
+        if key == 'sim':
+            continue
+
+        if isinstance(minmax2[key][0], bool):
+            color2 += np.where(df2['mp'] == key, 1, 0) * np.where(df2['value'] == value[1], 1, 0)
+        else:
+            color2 += np.where(df2['mp'] == key, 1, 0) * np.where(pd.to_numeric(df2['value']) > value[1], 3, 2)
+
     df['color'] = np.array(['black', 'white', 'green', 'yellow'])[color]
-    return time_latest, df
+    df2['color'] = np.array(['black', 'white', 'green', 'yellow'])[color2]
+    return time_latest, df, df2
 
 doc = curdoc()
-time_latest, df = makedf()
+time_latest, df, df2 = makedf()
 source = ColumnDataSource(df)
 
 # set up plot
 logger.info('Setting up plot')
 TOOLTIPS = [("value", "@value"), ("(ant_num, mp)", "(@ant_num, @mp)")]
 mplist = [mp for mp in list(minmax.keys()) if mp not in ignorelist]
-p = figure(plot_width=700, plot_height=1000, x_range=[str(aa) for aa in np.unique(df.ant_num)],
+p = figure(plot_width=1000, plot_height=1000, x_range=[str(aa) for aa in sorted(np.unique(df.ant_num).astype(int))],
            y_range=list(reversed(mplist)), y_axis_label='Monitor Point', x_axis_label='Antenna Number',
            tooltips=TOOLTIPS, toolbar_location=None, x_axis_location="above",
-           title="Antenna Monitor Points (loaded at MJD {0})".format(time_latest))
+           title="Antenna Monitor Points (started at MJD {0})".format(time_latest))
 p.rect(x='ant_num', y='mp', width=1, height=1, source=source,
        fill_color='color', alpha=0.5)
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
 
+source2 = ColumnDataSource(df2)
+mplist2 = [mp for mp in list(minmax2.keys()) if mp not in ignorelist]
+p2 = figure(plot_width=1000, plot_height=1000, x_range=[str(aa) for aa in sorted(np.unique(df2.ant_num).astype(int))],
+           y_range=list(reversed(mplist2)), y_axis_label='Monitor Point', x_axis_label='Antenna Number',
+           tooltips=TOOLTIPS, toolbar_location=None, x_axis_location="above",
+           title="BEB Monitor Points")
+p2.rect(x='ant_num', y='mp', width=1, height=1, source=source2,
+       fill_color='color', alpha=0.5)
+p2.xgrid.grid_line_color = None
+p2.ygrid.grid_line_color = None
+
+pall = column(p, p2)
+
 def update():
-    time_latest, df = makedf()
-    source.stream(df, rollover=len(df))  # updates each value
+    time_latest, df, df2 = makedf()
+    source.stream(df, rollover=len(df))  # updates each ant value
+    source2.stream(df2, rollover=len(df2))  # updates each beb value
 
 doc.add_periodic_callback(update, 5000)
 
-doc.add_root(p)
+doc.add_root(pall)
+doc.title = "DSA-110 Monitor Point Summary"
